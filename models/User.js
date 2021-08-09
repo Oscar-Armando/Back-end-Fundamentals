9a1654b241d2b5b81ee61f978cef2d2771e950cc
Armando <ing.robotica.armando@gmail.com>
2021-08-08 22:06:20 -0500

    users router

--- models/User.js
+++ models/User.js
@@ -3,20 +3,20 @@
 const mongoose  = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email: {type:String, required: true},
-    firstname: {type:String, required: true},
-    lastname: {type:String, required:true},
-    address: {type:String, required:true},
-    postalcode: {type:Number, required:true},
-    paymentmethod: {type:String, required:true},
-    phone: {type:Number, required:true}
+    email: {type: String, required: true, maxLength: 30},
+    firstname: {type: String, required: true, maxLength: 30},
+    lastname: {type: String, required:true, maxLength: 30},
+    address: {type: String, required:true, maxLength: 30 },
+    postalcode: {type: Number, required:true},
+    paymentmethod: {type: String, required:true},
+    phone: {type: Number, required:true}
 });
 
-UserSchema.path("email").validate(function (value){
-    return this.model("user").count({email: value}).them(function(count){
-    return count < 1;
-   })
-}, "Repeated email")
+//UserSchema.path("email").validate(function (value){
+    //return this.model("user").count({email: value}).them(function(count){
+    //return count < 1;
+   //})
+//}, "Repeated email")
 
 const User = mongoose.model("user", UserSchema);
 
